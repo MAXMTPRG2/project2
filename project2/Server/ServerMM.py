1f1c283db1bccbab97b74d496d433f1680f237e2
MAXMTPRG2 <maxwell.molnar@dcmail.ca>
2024-12-13 16:43:35 -0500

    Add files via upload
    I added a blinking LED for every time the server receives a new iteration of data and used threading events to set flags for the second thread to ensure a smooth exit of the server and joined the main and second thread in the main loop. Error handling was also improved to ensure that the thread didn't continue or throw errors if the Client exited or stopped transmitting data.

--- project2/Server/ServerMM.py
+++ project2/Server/ServerMM.py
@@ -17,7 +17,7 @@ import threading
 
 # Setup socket parameters
 s = socket.socket()
-host = '10.0.0.225'
+host = '10.0.0.225' #Local host or loopback address (for same machine)
 port = 8000
 s.bind((host, port))
 s.listen(5)
@@ -34,8 +34,8 @@ def receive_handler():
     data = json.loads(jsonReceived)
     return data
 
-def refresh_gui(window, jsondata, led_state):
-    '''Function to update the gui'''
+def refresh_gui(window, jsondata):
+    '''Function to update the gui, blink LED'''
     ret1 = jsondata["Core Temperature"]
     window[f'-CoreTemp-'].update(ret1)
     ret2 = jsondata["Core Voltage"]
@@ -49,58 +49,71 @@ def refresh_gui(window, jsondata, led_state):
     ret6 = jsondata["Iteration"]
     window[f'-Iteration-'].update(ret6)
     
-    led_indicator = CIRCLE if led_state else CIRCLE_OUTLINE
-    window[f'-LED0-'].update(led_indicator)
-
+    #Blink LED twice when data is updated
+    window[f'-LED0-'].update(CIRCLE)
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE_OUTLINE)
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE)
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE_OUTLINE)
+    time.sleep(0.10)
 
 CIRCLE = '\u2B24'
 CIRCLE_OUTLINE = '\u25EF'
 
+
 def main():
     '''Main program to start gui and run the second thread'''
-     try:
-        sg.theme('Light Blue') #Add colour to the GUI
+    try:
+        sg.theme('Light Blue')#Add colour to the GUI
         #Setup the layout, create spaces&keys for data to be updated
         layout = [[sg.Text('Incoming Data Display', font=('Helvetica', 16))],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreTemp-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreVolt-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreClock-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-voltRAM-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-memGPU-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-Iteration-'), sg.Push()],
+                [sg.Text('Core Temperature:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreTemp-'), sg.Push()],
+                [sg.Text('Core Voltage:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreVolt-'), sg.Push()],
+                [sg.Text('Core Frequency:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreClock-'), sg.Push()],
+                [sg.Text('RAM Voltage:'), sg.Push(), sg.Text('', size=(20,1), key='-voltRAM-'), sg.Push()],
+                [sg.Text('GPU Memory in Millions of Bytes:'), sg.Push(), sg.Text('', size=(20,1), key='-memGPU-'), sg.Push()],
+                [sg.Text('Iteration Number:'), sg.Push(), sg.Text('', size=(20,1), key='-Iteration-'), sg.Push()],
                 [sg.Text('Incoming data LED:', font=('Helvetica', 16))],
                 [sg.Push(), sg.Text(CIRCLE_OUTLINE, text_color='Red', key='-LED0-'), sg.Push()],
                 [sg.Button('Exit')]]
         
         window = sg.Window('Server Window - Max Molnar', layout, finalize=True)
         
-        def recieve_thread():
+        def recieve_thread(flag):
             '''Thread loop to handle the data receiving process'''
-            while True:
-                json_data = receive_handler()
+            try:
+                while not flag.is_set():
+                    json_data = receive_handler()
                 
-                if json_data:
-                    led_state = True
-                    refresh_gui(window, json_data, led_state)
-                time.sleep(2)
-                
-        t = threading.Thread(target=recieve_thread, daemon=True)
-        t.start()
+                    if json_data:
+                        refresh_gui(window, json_data)
+                    time.sleep(1.6)
+            except Exception:
+                print("Data transmit complete. ")
+                exit()
+        done_flag = threading.Event()       
+        thread1 = threading.Thread(target=recieve_thread, args=(done_flag,))
+        thread1.start()
         #Main loop for GUI
         while True:
             event, values = window.read()
             
             if event == sg.WINDOW_CLOSED or event == 'Exit':
                 break
-        
+        done_flag.set()
+        thread1.join()
+        print("Exiting...")
         window.close()
-     except Exception:
-         print("Error, Exiting...")
-         exit()
-#Main Program
+    except Exception:
+        print("Error, Exiting...")
+        exit()
+
 if __name__ == "__main__":
      try:
         main()
+        exit()
      except Exception:
          print("Error, Exiting...")
          exit()
