b1e1d42118b129c22065bd06d4f847cb1af64e23
MAXMTPRG2 <maxwell.molnar@dcmail.ca>
2024-12-13 18:07:22 -0500

    Add files via upload
    I almost forgot to add a keyboardInterrupt exception for a smooth exit upon entry of CTRL-C, so that exception is added now. Both files are fully commented and ready to mark.

--- project2/Client/ClientMM.py
+++ project2/Client/ClientMM.py
@@ -9,6 +9,18 @@
 # the Web or other sources is properly cited, giving
 # credit to the original author(s).
 
+# Program adapted from code/techniques provided by Professor Phil J
+
+# The sources I used:
+# https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_LED_Indicators_Text_Based.py
+# https://www.pythontutorial.net/python-concurrency/python-threading-event/
+# https://codingidol.com/how-to-get-output-from-subprocess-python/
+# https://www.nicm.dev/vcgencmd/
+
+# Connect to the server and collate and send 50 iterations of VCGENCMD command data
+# in JSON format, Update the GUI/LED every 2 seconds to illustrate connection,
+# client will not run on windows, only on raspberry Pi
+
 import subprocess
 import time
 import socket
@@ -22,76 +34,85 @@ IS_RPI = Path("/etc/rpi-issue").exists() #Used to check - Were using the pi
 if (IS_RPI):
     print("Correct Hardware")
 else:
-    print("Not on a Raspberry Pi, Exiting...")
+    print("Not on a Raspberry Pi, Exiting...") #Exit if not using the Pi
     exit()
 
 
 # Setup socket object and parameters
 s = socket.socket()
-host = '10.0.0.225' # Local host of the Pi
+host = '10.0.0.225' # Local host of the Pi, or loopback address
 port = 8000
 
 def collate_data(iteration):
     '''Function to get/collate vcgencmd command data'''
+    # Use subprocess to avoid interupting the GUI, remove characters from VCGENCMD command values
+    # and convert to float
     temp = float(subprocess.getoutput("vcgencmd measure_temp").split("=")[1].replace("'C", ""))
     voltage = float(subprocess.getoutput("vcgencmd measure_volts core").split("=")[1].replace("V", ""))
     clock = float(subprocess.getoutput("vcgencmd measure_clock core").split("=")[1])
     voltsram = float(subprocess.getoutput("vcgencmd measure_volts sdram_p").split("=")[1].replace("V", ""))
     memory = float(subprocess.getoutput("vcgencmd get_mem gpu").split("=")[1].replace("M", ""))
-    
+    # Formulate data into a dictionary and round to 1 decimal place
     data_dict = {"Core Temperature": round(temp,1), "Core Voltage": round(voltage,1), "Core Freq": round(clock,1),
                  "RAM Voltage": round(voltsram,1), "GPU Mem": round(memory,1), "Iteration": iteration}
+    #Convert to JSON format
     json_dict = json.dumps(data_dict)
-    return json_dict
+    return json_dict #Return JSON Data
 
 def send_to_server(jsonmessage):
     '''Function to send json data to the server'''
-    jsonbyte = bytearray(jsonmessage, "UTF-8")
-    s.send(jsonbyte)
+    jsonbyte = bytearray(jsonmessage, "UTF-8") #Convert data to BYTES using UTF-8
+    s.send(jsonbyte) # Send the data
     print("Data sent!")
     
     
 def vcgencmd_gui():
     '''Main Function to handle GUI and Iterations 0 - 50'''
+    # Add colour to the GUI 
     sg.theme('Light Blue')
     
-    CIRCLE = '\u2B24'              #  '⚫'
-    CIRCLE_OUTLINE = '\u25EF'      #  '⚪'
+    CIRCLE = '\u2B24'              #  Solid/LED ON
+    CIRCLE_OUTLINE = '\u25EF'      #  Blank/LED OFF
     
+    #Format the layout
     layout = [[sg.Text('Connection Indicator LED')],
             [sg.Push(), sg.Text(CIRCLE_OUTLINE, text_color='Red', key='-LED0-'), sg.Push()],
             [sg.Button('Exit')]]
     
-    
+    #Create the window
     window = sg.Window('Client Window - Max Molnar', layout, font='Any 16')
     try:
-        window.read(timeout=200)
-        s.connect((host, port))
-        window[f'-LED0-'].update(CIRCLE)
+        window.read(timeout=200) #Refresh GUI
+        s.connect((host, port)) # Begin connection
+        window[f'-LED0-'].update(CIRCLE) # If connection, turn LED on
         #Transmit data for 50 iterations
         for iteration in range(1,51):
-            jsondata = collate_data(iteration)
+            jsondata = collate_data(iteration) #Grab the data
             if jsondata:
-                event,values = window.read(timeout=200)
+                event,values = window.read(timeout=200) #Refresh GUI
                 if event == sg.WIN_CLOSED or event == 'Exit':
                     window.close()
                     break
-                window[f'-LED0-'].update(CIRCLE)
-                send_to_server(jsondata)
-            time.sleep(2)
-    except:
-        window.read(timeout=200)
-        window[f'-LED0-'].update(CIRCLE_OUTLINE)
-        print("Couldent make a connection")
+                window[f'-LED0-'].update(CIRCLE) # if connection is still active keep LED on
+                send_to_server(jsondata) # Send the data using by calling the send function
+            time.sleep(2) # Send iterations and update GUI every 2 seconds
+    except Exception: # If connection or server couldent be made/found
+        window.read(timeout=200)#Refresh GUI
+        window[f'-LED0-'].update(CIRCLE_OUTLINE) #Turn/keep Led off
+        print("Couldent make a connection") #Print error statement
 
 #Main Program
 if __name__ == "__main__":
     try:
-        vcgencmd_gui()
+        vcgencmd_gui() #Run the main program
+        # After 50 iterations of data close the socket and exit program
         s.close()
         print("Exiting...")
         exit()
-    except Exception:
+    except Exception: # General program error catch
         print("Exiting...")
         s.close()
+        exit()
+    except KeyboardInterrupt: #CTRL-C to quit
+        print("Exiting")
         exit()
--- project2/Server/ServerMM.py
+++ project2/Server/ServerMM.py
@@ -9,6 +9,18 @@
 # the Web or other sources is properly cited, giving
 # credit to the original author(s).
 
+# Program adapted from code/techniques provided by Professor Phil J
+
+# The sources I used:
+# https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_LED_Indicators_Text_Based.py
+# https://www.pythontutorial.net/python-concurrency/python-threading-event/
+# https://codingidol.com/how-to-get-output-from-subprocess-python/
+# https://www.nicm.dev/vcgencmd/
+
+
+# Receive 50 iterations of data and contnuously update a PySimpleGUI with the new VCGENCMD
+# data from the client wait for a new connection or Exit. 
+
 import socket
 import os, time
 import json
@@ -17,9 +29,9 @@ import threading
 
 # Setup socket parameters
 s = socket.socket()
-host = '10.0.0.225'
+host = '10.0.0.225' #Local host or loopback address (for same machine)
 port = 8000
-s.bind((host, port))
+s.bind((host, port)) # BInd the parameters
 s.listen(5)
 print("Server Listening...")
 
@@ -30,12 +42,14 @@ print("Got connection from ", addr)
 
 def receive_handler():
     '''Recieve and parse json data'''
-    jsonReceived = c.recv(1024)
-    data = json.loads(jsonReceived)
-    return data
+    jsonReceived = c.recv(1024) # Receive data
+    data = json.loads(jsonReceived) # Parse the data
+    return data #Return parsed data
 
-def refresh_gui(window, jsondata, led_state):
-    '''Function to update the gui'''
+def refresh_gui(window, jsondata):
+    '''Function to update the gui, blink LED'''
+    
+    # Grab data from the Dictionary and pass to GUI
     ret1 = jsondata["Core Temperature"]
     window[f'-CoreTemp-'].update(ret1)
     ret2 = jsondata["Core Voltage"]
@@ -49,58 +63,77 @@ def refresh_gui(window, jsondata, led_state):
     ret6 = jsondata["Iteration"]
     window[f'-Iteration-'].update(ret6)
     
-    led_indicator = CIRCLE if led_state else CIRCLE_OUTLINE
-    window[f'-LED0-'].update(led_indicator)
+    #Blink LED twice when data is updated
+    window[f'-LED0-'].update(CIRCLE)
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE_OUTLINE) # Cumulative sleep time of 0.4 seconds
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE)
+    time.sleep(0.10)
+    window[f'-LED0-'].update(CIRCLE_OUTLINE)
+    time.sleep(0.10)
 
+CIRCLE = '\u2B24' #Solid LED
+CIRCLE_OUTLINE = '\u25EF' #Blank/off LED
 
-CIRCLE = '\u2B24'
-CIRCLE_OUTLINE = '\u25EF'
 
 def main():
     '''Main program to start gui and run the second thread'''
-     try:
-        sg.theme('Light Blue') #Add colour to the GUI
+    try:
+        sg.theme('Light Blue')#Add colour to the GUI
         #Setup the layout, create spaces&keys for data to be updated
         layout = [[sg.Text('Incoming Data Display', font=('Helvetica', 16))],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreTemp-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreVolt-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-CoreClock-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-voltRAM-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-memGPU-'), sg.Push()],
-                [sg.Push(), sg.Text('', size=(20,1), key='-Iteration-'), sg.Push()],
+                [sg.Text('Core Temperature:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreTemp-'), sg.Push()],
+                [sg.Text('Core Voltage:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreVolt-'), sg.Push()],
+                [sg.Text('Core Frequency:'), sg.Push(), sg.Text('', size=(20,1), key='-CoreClock-'), sg.Push()],
+                [sg.Text('RAM Voltage:'), sg.Push(), sg.Text('', size=(20,1), key='-voltRAM-'), sg.Push()],
+                [sg.Text('GPU Memory in Millions of Bytes:'), sg.Push(), sg.Text('', size=(20,1), key='-memGPU-'), sg.Push()],
+                [sg.Text('Iteration Number:'), sg.Push(), sg.Text('', size=(20,1), key='-Iteration-'), sg.Push()],
                 [sg.Text('Incoming data LED:', font=('Helvetica', 16))],
                 [sg.Push(), sg.Text(CIRCLE_OUTLINE, text_color='Red', key='-LED0-'), sg.Push()],
                 [sg.Button('Exit')]]
-        
+        # Create the window
         window = sg.Window('Server Window - Max Molnar', layout, finalize=True)
         
-        def recieve_thread():
+        def recieve_thread(flag):
             '''Thread loop to handle the data receiving process'''
-            while True:
-                json_data = receive_handler()
+            try:
+                while not flag.is_set(): #Run thread while event is not set
+                    json_data = receive_handler() # Retrieve the data
                 
-                if json_data:
-                    led_state = True
-                    refresh_gui(window, json_data, led_state)
-                time.sleep(2)
-                
-        t = threading.Thread(target=recieve_thread, daemon=True)
-        t.start()
+                    if json_data:
+                        refresh_gui(window, json_data) # Update the GUI with new data
+                    time.sleep(1.6) # 1.6 seconds combines with the LED 0.4 seconds for sleep time of 2 seconds
+            except Exception:
+                # In case connection is interrupted or data is finished
+                print("Data transmit complete. ")
+                exit()
+        done_flag = threading.Event()  # Create a threading event to stop thread1     
+        thread1 = threading.Thread(target=recieve_thread, args=(done_flag,)) # Create the thread
+        thread1.start() # Run thread1
+        
         #Main loop for GUI
         while True:
-            event, values = window.read()
-            
+            event, values = window.read() #Refresh
+            # Exit or window close events
             if event == sg.WINDOW_CLOSED or event == 'Exit':
                 break
-        
+        # Set the threading event to end, join the main and second thread together
+        done_flag.set()
+        thread1.join()
+        print("Exiting...")
         window.close()
-     except Exception:
-         print("Error, Exiting...")
-         exit()
-#Main Program
+    except Exception: # Main error catch
+        print("Error, Exiting...")
+        exit()
+#Main program
 if __name__ == "__main__":
      try:
-        main()
-     except Exception:
+        main() # Run main
+        exit() # Then Exit
+     except Exception: # Overall error catch for the server
          print("Error, Exiting...")
+         exit()
+     except KeyboardInterrupt: #CTRL-C to quit
+         print("Exiting")
          exit()
